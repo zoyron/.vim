76c3a5d1b54057c03d6ad4dfbf556cf0624ae66c
zoyron <sgrarora3@gmail.com>
2021-11-29 14:29:42 +0530

    added other files too

---
+++ templates/skeleton.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define     endl '\n'
+#define     mod int(1e9) + 7
+using namespace std;
+
+void solve(){}
+
+int32_t main(){
+  int t = 1;
+  cin >> t;
+  while(t--)
+    solve();
+  return 0;
+}
+
